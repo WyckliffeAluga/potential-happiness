eb8d41db42e8a6a5a9903787e63088bef25cb7af
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-05-05 15:59:32 -0700

    self driving v0 updated

--- self-driving-car/map.py
+++ self-driving-car/map.py
@@ -16,7 +16,7 @@ from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProper
 from kivy.vector import Vector
 from kivy.clock import Clock
 
-# Importing the Dqn object from our AI in ai.py
+
 from ai_version3 import Dqn
 
 # Adding this line if we don't want the right click to put a red point

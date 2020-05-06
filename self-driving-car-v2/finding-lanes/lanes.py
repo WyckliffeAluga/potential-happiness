14882afe5fd649bcd4613b973d05abea20543108
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-05-05 20:04:23 -0700

    video capabilities added and optimized lines

--- self-driving-car-v2/finding-lanes/lanes.py
+++ self-driving-car-v2/finding-lanes/lanes.py
@@ -8,9 +8,6 @@ Created on Tue May  5 16:28:38 2020
 
 import cv2 
 import numpy as np
-import matplotlib.pyplot as plt
-
-image = cv2.imread('test_image.jpg')
 
 
 # edge detection (sharp changes in intensity in adjacent pixels )
@@ -75,13 +72,19 @@ class EdgeDetection():
         return cv2.HoughLinesP(self.bitwise_cropping(), 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
     
         
-    def display_lines(self) : 
-        
+    def display_lines(self, lines) : 
+        """
+        Displays lanes on a black image
+
+        Returns
+        -------
+        line_image : TYPE
+            DESCRIPTION.
+
+        """
         line_image = np.zeros_like(self.img)
         
-        # initialize the class 
-        
-        lines = self.line_detection()
+    
         
         if lines is not None: 
             # loop through the list
@@ -92,15 +95,78 @@ class EdgeDetection():
         return line_image
     
     def lanes(self) :
+        """
         
-        return cv2.addWeighted(self.display_lines(), 0.8, self.img, 1 , 1)
-            
-    def show(self, imageMatrix):
-        cv2.imshow("result", imageMatrix)
-        cv2.waitKey(15)
 
+        Returns
+        -------
+        lanes on the colored image
+
+        """
         
-eyes = EdgeDetection(image)
+        return cv2.addWeighted(self.display_lines(self.line_detection()), 0.8, self.img, 1 , 1)
+    
+    def make_cordinates(self , line_parameters): 
+        
+        slope, intercept = line_parameters
+        y1 = self.img.shape[0]
+        y2 = int(y1 * (3/5))
+        x1 = int((y1 - intercept) / slope)
+        x2 = int((y2 - intercept) / slope)
+        
+        return np.array([x1, y1, x2, y2])
+  
+        
+    def average_slope_intercepts(self):
+        
+        left_fit = []
+        right_fit = []
+        
+        for line in self.line_detection() : 
+           x1, y1,x2, y2 =  line.reshape(4)
+           parameters = np.polyfit((x1, x2), (y1, y2) , 1)
+           slope = parameters[0]
+           intercept = parameters[1]
+           
+           if slope < 0 : # negative slope 
+               left_fit.append((slope, intercept))
+           else:
+               right_fit.append((slope, intercept))
+                   
+       
+        left_fit_average = np.average(left_fit, axis=0)
+        right_fit_average = np.average(right_fit, axis=0)
+        
+        left_line  = self.make_cordinates(left_fit_average)
+        right_line = self.make_cordinates(right_fit_average)
+        
+        return np.array([left_line, right_line])
+        
+    def averaged_lines_image(self): 
+        
+        averaged_lines = self.average_slope_intercepts()
+        line_image = self.display_lines(averaged_lines)
+       
+        return cv2.addWeighted(line_image, 0.8, self.img, 1, 1)
+        
+        
+    def show(self, imageMatrix):
+      cv2.imshow("result", imageMatrix)
+     
+        
+# video capture object 
+cap = cv2.VideoCapture("test2.mp4")
+
+while(cap.isOpened()): 
+    _, frame = cap.read()       
+    eyes = EdgeDetection(frame)
+    eyes.show(eyes.averaged_lines_image())
+    
+    if cv2.waitKey(1) == ord("q"): 
+        break 
+    
+cap.release()
+cv2.destroyAllWindows()
 
 
 

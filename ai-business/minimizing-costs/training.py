080198b3e0ee42525f5fdf69566c8f69eb890f40
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-05-23 01:25:13 -0700

    Update training.py

--- ai-business/minimizing-costs/training.py
+++ ai-business/minimizing-costs/training.py
@@ -27,3 +27,14 @@ max_memory = 3000
 batch_size = 512
 temperature_step = 1.5
 
+# Build the enviroment
+env = env.Env(temp_range=(10.0, 24.0), initial_month=0, initial_number_of_users=20, initial_rate_data=30)
+
+# launch network
+brain = network.Network(learning_rate=0.0001, number_actions=number_actions)
+
+# launch q_learning
+dqn = q_learning.DQN(max_memory=max_memory, discount=0.9)
+
+# mode
+train = True

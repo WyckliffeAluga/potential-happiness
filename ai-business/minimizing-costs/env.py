95e163986ce042f6bcfd361bf92f7eb53c207979
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-05-24 14:21:33 -0700

    Update env.py

--- ai-business/minimizing-costs/env.py
+++ ai-business/minimizing-costs/env.py
@@ -1,26 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri May 22 22:32:28 2020
-
+Created on Fri May 22 22:32:28 202
 @author: wyckliffe
 """
-
 import numpy as  np
 
-
 # built the enviroment
-
 class Env:
-
     # initialize the environme nt
-
     def __init__(self, optimal_temperature = (18.0, 24.0), initial_month = 0, initial_number_users = 10, initial_rate_data = 60):
-
         self.monthly_atmospheric_temperatures = [1.0, 5.0, 7.0, 10.0, 11.0, 20.0, 23.0, 24.0, 22.0, 10.0, 5.0, 1.0]
         self.initial_month = initial_month
         self.atmospheric_temperature = self.monthly_atmospheric_temperatures[initial_month]
         self.optimal_temperature = optimal_temperature
-
         self.min_temperature = -20
         self.max_temperature = 80
         self.min_number_users = 10
@@ -29,12 +21,10 @@ class Env:
         self.min_rate_data = 20
         self.max_rate_data = 300
         self.max_update_data = 10
-
         self.initial_number_users = initial_number_users
         self.current_number_users = initial_number_users
         self.initial_rate_data = initial_rate_data
         self.current_rate_data = initial_rate_data
-
         self.intrinsic_temperature = self.atmospheric_temperature + 1.25 * self.current_number_users + 1.25 * self.current_rate_data
         self.temperature_ai = self.intrinsic_temperature
         self.temperature_noai = (self.optimal_temperature[0] + self.optimal_temperature[1]) / 2.0
@@ -44,26 +34,21 @@ class Env:
         self.game_over = 0
         self.train = 1
 
-
     def update(self, direction, energy_ai, month):
-
         # Compute the energy spent by the server's cooling system when there is no AI
         energy_noai = 0
-
         if (self.temperature_noai < self.optimal_temperature[0]):
             energy_noai = self.optimal_temperature[0] - self.temperature_noai
             self.temperature_noai = self.optimal_temperature[0]
         elif (self.temperature_noai > self.optimal_temperature[1]):
             energy_noai = self.temperature_noai - self.optimal_temperature[1]
             self.temperature_noai = self.optimal_temperature[1]
-
         # Compute the Reward
         self.reward = energy_noai - energy_ai
         # Scale the Reward to allow for smooth computation of the Network
         self.reward = 1e-3 * self.reward
 
-
-        # Updata the atmospheric temperature
+        # Update the atmospheric temperature
         self.atmospheric_temperature = self.monthly_atmospheric_temperatures[month]
 
         # Update the number of users
@@ -98,7 +83,6 @@ class Env:
         self.temperature_noai += delta_intrinsic_temperature
 
         # Game over!!!!!
-
         if (self.temperature_ai < self.min_temperature):
             if (self.train == 1):
                 self.game_over = 1
@@ -125,11 +109,9 @@ class Env:
         scaled_rate_data = (self.current_rate_data - self.min_rate_data) / (self.max_rate_data - self.min_rate_data)
         next_state = np.matrix([scaled_temperature_ai, scaled_number_users, scaled_rate_data])
 
-
         return next_state, self.reward, self.game_over
 
-    # reset the environemtn
-
+    # reset the environmrnt
     def reset(self, new_month):
 
         self.atmospheric_temperature = self.monthly_atmospheric_temperatures[new_month]
@@ -144,7 +126,6 @@ class Env:
         self.reward = 0.0
         self.game_over = 0
         self.train = 1
-
 
     def observe(self):
 

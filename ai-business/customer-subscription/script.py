1d578220af81ff97ff8b9c1f1e818898fe5f2827
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-05-24 00:55:58 -0700

    Create script.py

---
+++ ai-business/customer-subscription/script.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 24 00:42:03 2020
+
+@author: wyckliffe
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from dateutil import parser
+
+
+data = pd.read_csv('appdata10.csv')

85a450119b5b577480f062693c27790937d93125
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-05-23 22:22:01 -0700

    Update fashion.py

--- ai-business/fashion-classification/fashion.py
+++ ai-business/fashion-classification/fashion.py
@@ -5,4 +5,30 @@ Created on Sat May 23 21:54:20 2020
 @author: wyckliffe
 """
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+class Fashion :
+
+    def __init__(self) :
+
+        train = pd.read_csv('fashion-mnist_train.csv', sep=',')
+        valid = pd.read_csv('fashion-mnist_test.csv' , sep=',')
+
+        assert(train.shape[1] == valid.shape[1])
+
+        self.train = np.array(train, dtype='float32')
+        self.valid  = np.array(valid, dtype='float32')
+
+    def show(self) :
+
+        row = np.random.randint(0, len(self.train) + 1)
+        plt.imshow(self.train[row, 1:].reshape(28,28), cmap=plt.get_cmap('gray'))
+
+
+if __name__ == '__main__' :
+
+    f = Fashion()
 

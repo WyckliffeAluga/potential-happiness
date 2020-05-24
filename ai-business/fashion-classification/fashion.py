8ef80297eb9d77571ee6de2b2dfccba20bc5193b
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-05-24 14:25:29 -0700

    Update fashion.py

--- ai-business/fashion-classification/fashion.py
+++ ai-business/fashion-classification/fashion.py
@@ -16,12 +16,11 @@ from keras.models import Sequential , load_model
 from keras.optimizers import  Adam
 
 class Fashion :
-
+    
     def __init__(self) :
 
         train = pd.read_csv('fashion-mnist_train.csv', sep=',')
         valid = pd.read_csv('fashion-mnist_test.csv' , sep=',')
-
         assert(train.shape[1] == valid.shape[1])
 
         self.train = np.array(train, dtype='float32')
@@ -29,7 +28,6 @@ class Fashion :
         self.x_test = self.valid[:, 1:] / 255
         self.y_test = self.valid[:, 0]
         self.x_test = self.x_test.reshape(self.x_test.shape[0], *(28, 28 ,1))
-
 
     def show(self) :
 
@@ -69,13 +67,10 @@ class Fashion :
     def model(self) :
         x_train = self.train[:, 1:] / 255
         y_train = self.train[:, 0]
-
         self.x_test = self.valid[:, 1:] / 255
         self.y_test = self.valid[:, 0]
-
         x_train , x_validate, y_train , y_validate = train_test_split(x_train, y_train,
                                                                       test_size=0.2, random_state=42)
-
         x_train = x_train.reshape(x_train.shape[0], *(28, 28, 1))
         self.x_test = self.x_test.reshape(self.x_test.shape[0], *(28, 28 ,1))
         x_validate = x_validate.reshape(x_validate.shape[0], *(28, 28, 1))
@@ -100,23 +95,12 @@ class Fashion :
 
         model = load_model('model.h5')
         predictions = model.predict_classes(f.x_test)
-
-
         image = np.random.randint(0, len(f.x_test) + 1)
-
         plt.imshow(self.x_test[image].reshape(28, 28), cmap=plt.get_cmap('gray'))
         plt.title("Prediction: {} \n True Class: {}".format(self.label_encoder(predictions[image]), self.label_encoder(self.y_test[image])))
 
-
-
-
 if __name__ == '__main__' :
-
     f = Fashion()
-
     model = load_model('model.h5')
    # evaluation = model.evaluate(f.x_test, f.y_test)
    # print('Test accuracy : {:.3f}'.format(evaluation[1]))
-
-
-

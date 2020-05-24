f1192f5f1bea6025a03c0f660e4ed50d15f145d8
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-05-24 14:22:18 -0700

    Update q_learning.py

--- ai-business/minimizing-costs/q_learning.py
+++ ai-business/minimizing-costs/q_learning.py
@@ -4,29 +4,20 @@ Created on Sat May 23 00:38:55 2020
 
 @author: wyckliffe
 """
-
-
-
-
 import numpy as np
 
-
 class DQN:
-
     def __init__(self, max_memory = 100, discount = 0.9):
-
         self.memory = list()
         self.max_memory = max_memory
         self.discount = discount
 
     def remember(self, transition, game_over):
-
         self.memory.append([transition, game_over])
         if len(self.memory) > self.max_memory:
             del self.memory[0]
 
     def get_batch(self, model, batch_size = 10):
-
         len_memory = len(self.memory)
         num_inputs = self.memory[0][0][0].shape[1]
         num_outputs = model.output_shape[-1]

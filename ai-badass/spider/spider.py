e77ce5b4c2b4d420a31fce676eb25da2dff59644
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-05-25 00:45:27 -0700

    Update spider.py

--- ai-badass/spider/spider.py
+++ ai-badass/spider/spider.py
@@ -75,3 +75,43 @@ class Actor(nn.Module):
     x = self.max_action * torch.tanh(self.layer_3(x))
 
     return x
+
+# create the critic class
+# this class has two neural networks that will be build simultenous
+class Critic(nn.Module):
+
+  def __init__(self, state_dim, action_dim):
+
+    super(Critic, self).__init__()
+    # first Critic neural network
+    self.layer_1 = nn.Linear(state_dim + action_dim, 400)
+    self.layer_2 = nn.Linear(400, 300)
+    self.layer_3 = nn.Linear(300, 1)
+
+    # second Critic neural network
+    self.layer_4 = nn.Linear(state_dim + action_dim, 400)
+    self.layer_5 = nn.Linear(400, 300)
+    self.layer_6 = nn.Linear(300, 1)
+
+  def forward(self, x, u):
+
+    xu = torch.cat([x, u], 1)
+
+    # Forward-Propagation on the first Neural Network
+    x1 = F.relu(self.layer_1(xu))
+    x1 = F.relu(self.layer_2(x1))
+    x1 = self.layer_3(x1)
+
+    # Forward-Propagation on the second Neural Network
+    x2 = F.relu(self.layer_4(xu))
+    x2 = F.relu(self.layer_5(x2))
+    x2 = self.layer_6(x2)
+    return x1, x2
+
+  def Q1(self, x, u):
+
+    xu = torch.cat([x, u], 1)
+    x1 = F.relu(self.layer_1(xu))
+    x1 = F.relu(self.layer_2(x1))
+    x1 = self.layer_3(x1)
+    return x1

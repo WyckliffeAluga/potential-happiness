f4b9429872415f2be029ac76f86e696185e57b21
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-05-24 21:43:34 -0700

    Update cheetah.py

--- ai-badass/cheetah/cheetah.py
+++ ai-badass/cheetah/cheetah.py
@@ -66,3 +66,35 @@ class Actor(nn.Module):
     x = F.relu(self.layer_2(x))
     x = torch.tanh(self.later_3(x)) * self.max_action
     return x
+
+
+class Critic(nn.Module) :
+
+  def __init__(self, state_dim, action_dim ) :
+    super(Critic, self).__init__()
+
+    # first critc NN
+    self.layer_1 = nn.Linear(state_dim + action_dim, 400, bias=False)
+    self.layer_2 = nn.Linear(400, 300, bias=False)
+    self.layer_3 = nn.Linear(300, 1)
+
+    # second critic NN
+    self.layer_4 = nn.Linear(state_dim + action_dim, 400, bias=False)
+    self.layer_5 = nn.Linear(400, 300, bias=False)
+    self.layer_6 = nn.Linear(300, 1)
+
+  def forward(self, x, u) :
+
+    xu = torch.cat([x, u], 1)
+    # first critic NN
+    x1 = F.relu(self.layer_1(xu))
+    x1 = F.relu(self.layer_2(x1))
+    x1 = self.layer_3(x1)
+    # second critic NN
+    x2 = F.relu(self.layer_4(xu))
+    x2 = F.relu(self.layer_5(x2))
+    x2 = self.layer_6(x2)
+
+    return x1, x2
+
+

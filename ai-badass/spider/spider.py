82cc202c5aff9e7752220c263a674ee94c6f2f31
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-05-25 00:42:33 -0700

    Update spider.py

--- ai-badass/spider/spider.py
+++ ai-badass/spider/spider.py
@@ -55,3 +55,23 @@ class ReplayBuffer:
       batch_dones.append(np.array(done, copy=False))
 
     return np.array(batch_states), np.array(batch_next_states), np.array(batch_actions), np.array(batch_rewards).reshape(-1, 1), np.array(batch_dones).reshape(-1, 1)
+
+
+# create the actor class
+class Actor(nn.Module):
+
+  def __init__(self, state_dim, action_dim, max_action):
+
+    super(Actor, self).__init__()
+    self.layer_1 = nn.Linear(state_dim, 400)
+    self.layer_2 = nn.Linear(400, 300)
+    self.layer_3 = nn.Linear(300, action_dim)
+    self.max_action = max_action
+
+  def forward(self, x):
+
+    x = F.relu(self.layer_1(x))
+    x = F.relu(self.layer_2(x))
+    x = self.max_action * torch.tanh(self.layer_3(x))
+
+    return x

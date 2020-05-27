e90cddf970908ef26b9cebff207f8492b245553e
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-05-26 21:15:03 -0700

    cheateh files

---
+++ ai-badass/cheetah/inference.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 26 18:29:40 2020
+
+@author: wyckliffe
+"""
+
+
+import os
+import time
+import numpy as np
+import pybullet_envs
+import gym
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from gym import wrappers
+
+
+class Actor(nn.Module):
+
+  def __init__(self, state_dim, action_dim, max_action):
+    super(Actor, self).__init__()
+    self.layer_1 = nn.Linear(state_dim, 400)
+    self.layer_2 = nn.Linear(400, 300)
+    self.layer_3 = nn.Linear(300, action_dim)
+    self.max_action = max_action
+
+  def forward(self, x):
+    x = F.relu(self.layer_1(x))
+    x = F.relu(self.layer_2(x))
+    x = self.max_action * torch.tanh(self.layer_3(x))
+    return x
+
+class Critic(nn.Module):
+
+  def __init__(self, state_dim, action_dim):
+    super(Critic, self).__init__()
+
+    # define the first Critic neural network
+    self.layer_1 = nn.Linear(state_dim + action_dim, 400)
+    self.layer_2 = nn.Linear(400, 300)
+    self.layer_3 = nn.Linear(300, 1)
+
+    # define the second Critic neural network
+    self.layer_4 = nn.Linear(state_dim + action_dim, 400)
+    self.layer_5 = nn.Linear(400, 300)
+    self.layer_6 = nn.Linear(300, 1)
+
+  def forward(self, x, u):
+    xu = torch.cat([x, u], 1)
+
+    # Forward-Propagation on the first Critic Neural Network
+    x1 = F.relu(self.layer_1(xu))
+    x1 = F.relu(self.layer_2(x1))
+    x1 = self.layer_3(x1)
+
+    # Forward-Propagation on the second Critic Neural Network
+    x2 = F.relu(self.layer_4(xu))
+    x2 = F.relu(self.layer_5(x2))
+    x2 = self.layer_6(x2)
+    return x1, x2
+
+  def Q1(self, x, u):
+    xu = torch.cat([x, u], 1)
+    x1 = F.relu(self.layer_1(xu))
+    x1 = F.relu(self.layer_2(x1))
+    x1 = self.layer_3(x1)
+    return x1
+
+# Selecting the device (CPU or GPU)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+#
+
+class TD3:
+
+  def __init__(self, state_dim, action_dim, max_action):
+
+    self.actor = Actor(state_dim, action_dim, max_action).to(device)
+    self.actor_target = Actor(state_dim, action_dim, max_action).to(device)
+    self.actor_target.load_state_dict(self.actor.state_dict())
+    self.actor_optimizer = torch.optim.Adam(self.actor.parameters())
+
+    self.critic = Critic(state_dim, action_dim).to(device)
+    self.critic_target = Critic(state_dim, action_dim).to(device)
+    self.critic_target.load_state_dict(self.critic.state_dict())
+    self.critic_optimizer = torch.optim.Adam(self.critic.parameters())
+
+    self.max_action = max_action
+
+  def select_action(self, state):
+    state = torch.Tensor(state.reshape(1, -1)).to(device)
+    return self.actor(state).cpu().data.numpy().flatten()
+
+  def train(self, replay_buffer, iterations, batch_size=100, discount=0.99, tau=0.005, policy_noise=0.2, noise_clip=0.5, policy_freq=2):
+
+    for it in range(iterations):
+
+      batch_states, batch_next_states, batch_actions, batch_rewards, batch_dones = replay_buffer.sample(batch_size)
+      state = torch.Tensor(batch_states).to(device)
+      next_state = torch.Tensor(batch_next_states).to(device)
+      action = torch.Tensor(batch_actions).to(device)
+      reward = torch.Tensor(batch_rewards).to(device)
+      done = torch.Tensor(batch_dones).to(device)
+
+      next_action = self.actor_target(next_state)
+
+      noise = torch.Tensor(batch_actions).data.normal_(0, policy_noise).to(device)
+      noise = noise.clamp(-noise_clip, noise_clip)
+      next_action = (next_action + noise).clamp(-self.max_action, self.max_action)
+
+      tq1, tq2 = self.critic_target(next_state, next_action)
+
+      q = torch.min(tq1, tq2)
+
+      Qt = reward + ((1 - done) * discount * q).detach()
+
+      q1, q2 = self.critic(state, action)
+
+      loss = F.mse_loss(q1, Qt) + F.mse_loss(q2, Qt)
+
+      self.critic_optimizer.zero_grad()
+      loss.backward()
+      self.critic_optimizer.step()
+
+      if it % policy_freq == 0:
+
+        actor_loss = -self.critic.Q1(state, self.actor(state)).mean()
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+
+        for param, target_param in zip(self.critic.parameters(), self.critic_target.parameters()):
+          target_param.data.copy_(tau * param.data + (1 - tau) * target_param.data)
+
+        for param, target_param in zip(self.actor.parameters(), self.actor_target.parameters()):
+          target_param.data.copy_(tau * param.data + (1 - tau) * target_param.data)
+
+  def save(self, filename, directory):
+    torch.save(self.actor.state_dict(), '%s/%s_actor.pth' % (directory, filename))
+    torch.save(self.critic.state_dict(), '%s/%s_critic.pth' % (directory, filename))
+
+  def load(self, filename, directory):
+    self.actor.load_state_dict(torch.load('%s/%s_actor.pth' % (directory, filename)))
+    self.critic.load_state_dict(torch.load('%s/%s_critic.pth' % (directory, filename)))
+
+
+def evaluate_policy(policy, eval_episodes=10):
+  avg_reward = 0.
+
+  for _ in range(eval_episodes):
+    obs = env.reset()
+    done = False
+    while not done:
+      action = policy.select_action(np.array(obs))
+      obs, reward, done, _ = env.step(action)
+      avg_reward += reward
+  avg_reward /= eval_episodes
+  print ("---------------------------------------")
+  print ("Average Reward over the Evaluation Step: %f" % (avg_reward))
+  print ("---------------------------------------")
+  return avg_reward
+
+env_name = "HalfCheetahBulletEnv-v0"
+seed = 0
+
+file_name = "%s_%s_%s" % ("TD3", env_name, str(seed))
+print ("---------------------------------------")
+print ("Settings: %s" % (file_name))
+print ("---------------------------------------")
+
+eval_episodes = 10
+save_env_vid = True
+env = gym.make(env_name)
+max_episode_steps = env._max_episode_steps
+
+if save_env_vid:
+  env = wrappers.Monitor(env, monitor_dir, force = True)
+  env.reset()
+
+env.seed(seed)
+torch.manual_seed(seed)
+np.random.seed(seed)
+state_dim = env.observation_space.shape[0]
+action_dim = env.action_space.shape[0]
+max_action = float(env.action_space.high[0])
+policy = TD3(state_dim, action_dim, max_action)
+policy.load(file_name, './pytorch_models/')
+_ = evaluate_policy(policy, eval_episodes=eval_episodes)

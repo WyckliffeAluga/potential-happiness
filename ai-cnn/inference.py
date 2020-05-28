670444b87a3b3f527b92c7ab1b2d32c921eb2309
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-05-28 16:43:12 -0700

    Update inference.py

--- ai-cnn/inference.py
+++ ai-cnn/inference.py
@@ -7,26 +7,16 @@ Created on Thu May 28 16:25:03 2020
 
 from keras.models import load_model
 from keras.preprocessing import image
-from keras.preprocessing.image import ImageDataGenerator
 
 import numpy as np
 
 
-train_datagen = ImageDataGenerator(rescale = 1./255,
-                                   shear_range = 0.2,
-                                   zoom_range = 0.2,
-                                   horizontal_flip = True)
-
-training_set = train_datagen.flow_from_directory('dataset/training_set',
-                                                 target_size = (64, 64),
-                                                 batch_size = 32,
-                                                 class_mode = 'binary')
 model = load_model('model.h5')
 test_image = image.load_img('dataset/single_prediction/cat_or_dog_2.jpg', target_size = (64, 64))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 result = model.predict(test_image)
-training_set.class_indices
+#training_set.class_indices
 
 if result[0][0] == 1:
     prediction = 'dog'

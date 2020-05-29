b2badc0e09a980494b717ba9b2fb8cbe3876ff56
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-05-28 19:33:05 -0700

    Create rnn.py

---
+++ ai-deep-learning/ai-rnn/rnn.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 28 18:56:02 2020
+
+@author: wyckliffe
+"""
+
+
+import pandas as pd
+
+
+
+train = pd.read_csv("Google_Stock_Price_Train.csv")
+test  = pd.read_csv("Google_Stock_Price_Test.csv")

3d4c9208bc2e61554da29c0df315895d90966cb9
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-05-28 22:20:57 -0700

    Update rnn.py

--- ai-deep-learning/ai-rnn/rnn.py
+++ ai-deep-learning/ai-rnn/rnn.py
@@ -6,9 +6,84 @@ Created on Thu May 28 18:56:02 2020
 """
 
 
+
+import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential , load_model
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+
+dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
+training_set = dataset_train.iloc[:, 1:2].values
 
 
+sc = MinMaxScaler(feature_range = (0, 1))
+training_set_scaled = sc.fit_transform(training_set)
 
-train = pd.read_csv("Google_Stock_Price_Train.csv")
-test  = pd.read_csv("Google_Stock_Price_Test.csv")
+
+X_train = []
+y_train = []
+for i in range(60, 1258):
+    X_train.append(training_set_scaled[i-60:i, 0])
+    y_train.append(training_set_scaled[i, 0])
+X_train, y_train = np.array(X_train), np.array(y_train)
+
+# reshape to add  a dimension
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+
+
+model = Sequential()
+model.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+model.add(Dropout(0.2))
+model.add(LSTM(units = 50, return_sequences = True))
+model.add(Dropout(0.2))
+model.add(LSTM(units = 50, return_sequences = True))
+model.add(Dropout(0.2))
+model.add(LSTM(units = 50))
+model.add(Dropout(0.2))
+model.add(Dense(units = 1))
+model.compile(optimizer = 'adam', loss = 'mean_squared_error')
+#model.fit(X_train, y_train, epochs = 100, batch_size = 32)
+
+#model.save('model.h5')
+
+# get the read stock data
+dataset_test = pd.read_csv('Google_Stock_Price_Test.csv')
+real_stock_price = dataset_test.iloc[:, 1:2].values
+
+# make prediction
+dataset_total = pd.concat((dataset_train['Open'], dataset_test['Open']), axis = 0)
+inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
+inputs = inputs.reshape(-1,1)
+inputs = sc.transform(inputs)
+
+X_test = []
+
+for i in range(60, 80):
+    X_test.append(inputs[i-60:i, 0])
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+
+model = load_model('model.h5')
+predicted_stock_price = model.predict(X_test)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+
+# Visualising the results
+plt.plot(real_stock_price, color = 'red', label = 'Real Google Stock Price')
+plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted Google Stock Price')
+plt.title('Google Stock Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('Google Stock Price')
+plt.legend()
+plt.show()
+
+diff = real_stock_price - predicted_stock_price
+plt.plot(diff, label='difference in prediction')
+plt.title('How well did rnn perfom?')
+plt.xlabel('Time')
+plt.ylabel('Difference in prediction')
+plt.legend()
+plt.show()

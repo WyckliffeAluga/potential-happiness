dfae123db9f249226eb2cadf4ec863c94bed0889
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-05-29 21:39:46 -0700

    Update ann.py

--- ai-deep-learning/ai-ann/churn_rate_modelling/ann.py
+++ ai-deep-learning/ai-ann/churn_rate_modelling/ann.py
@@ -5,70 +5,50 @@ Created on Mon May 11 14:35:16 2020
 @author: wyckliffe
 """
 
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import confusion_matrix
-
 import keras
-from keras.models import Sequential , Dropout
+from keras.models import Sequential
 from keras.layers import Dense
 
-data = pd.read_csv('bank_data.csv')
+# import the dataset
+dataset = pd.read_csv('bank_data.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
 
-X = data.iloc[:, 3:13]
-y = data.iloc[:, 13]
+# encode categorical data
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:]
 
-X = pd.get_dummies(X)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-scaler = StandardScaler()
+# scale the features
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
 
-x_train , x_test, y_train , y_test = train_test_split(X, y, test_size=0.2, random_state=123)
-
-x_train = scaler.fit_transform(x_train)
-x_test = scaler.fit_transform(x_test)
 
 model = Sequential()
+model.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+model.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+model.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+model.fit(X_train, y_train, batch_size = 10, epochs = 100)
 
-model.add(Dense(output_dim = 6, init='uniform', activation='relu', input_dim=13))
-model.add(Dense(output_dim = 6, init='uniform', activation='relu'))
-model.add(Dense(output_dim = 1, init='uniform', activation='sigmoid'))
-
-model.compile(optimizer='adam', loss='binary_crossentropy',
-              metrics=['accuracy'])
-
-model.fit(x_train, y_train, epochs=50, shuffle=1, batch_size=10)
-
-y_pred = model.predict(x_test)
+# Predicting the Test set results
+y_pred = model.predict(X_test)
 y_pred = (y_pred > 0.5)
 
-new_customer = np.array([[600, 0., 0, 0, 1, 40, 3, 60000, 2, 1, 0, 0, 50000]])
-new_customer = scaler.fit_transform(new_customer)
-new_prediction = model.predict(new_customer)
-
-
-from keras.wrappers.scikit_learn import KerasClassifier
-from sklearn.model_selection import cross_val_score
-
-
-def model():
-    model = Sequential()
-
-    model.add(Dense(output_dim = 6, init='uniform', activation='relu', input_dim=13))
-    model.add(Dense(output_dim = 6, init='uniform', activation='relu'))
-    model.add(Dense(output_dim = 1, init='uniform', activation='sigmoid'))
-
-    model.compile(optimizer='adam', loss='binary_crossentropy',
-              metrics=['accuracy'])
-
-    return model
-
-classifier = KerasClassifier(build_fn = model, batch_size=10, epochs=50)
-accuracies = cross_val_score(estimator=classifier, X=x_train, y=y_train, cv=10, n_jobs=-1)
-
-mean = accuracies.mean()
-variance = accuracies.std()
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
 

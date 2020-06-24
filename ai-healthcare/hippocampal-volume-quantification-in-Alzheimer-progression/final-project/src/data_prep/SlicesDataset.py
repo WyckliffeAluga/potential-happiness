a87572adfc1df97ddbe4b2958ecfb56097eac299
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-06-23 19:27:19 -0700

    Update SlicesDataset.py

--- ai-healthcare/hippocampal-volume-quantification-in-Alzheimer-progression/final-project/src/data_prep/SlicesDataset.py
+++ ai-healthcare/hippocampal-volume-quantification-in-Alzheimer-progression/final-project/src/data_prep/SlicesDataset.py
@@ -23,7 +23,7 @@ class SlicesDataset(Dataset):
         """
         This method is called by PyTorch DataLoader class to return a sample with id idx
 
-        Arguments: 
+        Arguments:
             idx {int} -- id of sample
 
         Returns:
@@ -36,18 +36,22 @@ class SlicesDataset(Dataset):
         # You could implement caching strategy here if dataset is too large to fit
         # in memory entirely
         # Also this would be the place to call transforms if data augmentation is used
-        
+
         # TASK: Create two new keys in the "sample" dictionary, named "image" and "seg"
-        # The values are 3D Torch Tensors with image and label data respectively. 
+        # The values are 3D Torch Tensors with image and label data respectively.
         # First dimension is size 1, and last two hold the voxel data from the respective
-        # slices. Write code that stores the 2D slice data in the last 2 dimensions of the 3D Tensors. 
+        # slices. Write code that stores the 2D slice data in the last 2 dimensions of the 3D Tensors.
         # Your tensor needs to be of shape [1, patch_size, patch_size]
         # Don't forget that you need to put a Torch Tensor into your dictionary element's value
         # Hint: your 3D data sits in self.data variable, the id of the 3D volume from data array
-        # and the slice number are in the slc variable. 
-        # Hint2: You can use None notation like so: arr[None, :] to add size-1 
+        # and the slice number are in the slc variable.
+        # Hint2: You can use None notation like so: arr[None, :] to add size-1
         # dimension to a Numpy array
-        # <YOUR CODE GOES HERE>
+        img = self.data[slc[0]]['image'][slc[1]]
+        seg = self.data[slc[0]]['seg'][slc[1]][None,:]
+
+        sample['image'] = torch.from_numpy(img).unsqueeze(0).floor().to('cuda')
+        sample['seg'] = torch.from_numpy(seg).long().to('cuda')
 
         return sample
 

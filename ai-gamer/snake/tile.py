feabbab41cb329f03c240980c1bfac966f7fd9a9
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-07-05 17:35:30 -0700

    Create tile.py

---
+++ ai-gamer/snake/tile.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  5 14:10:59 2020
+
+@author: wyckliffe
+"""
+
+
+
+import numpy as np
+
+class Tile():
+    empty = " "
+    snake = "x"
+    fruit = "$"
+    wall = "#"
+
+    @staticmethod
+    def grayscale(tile):
+        if tile == Tile.empty:
+            return np.float32(0)#np.uint8(255)
+        elif tile == Tile.fruit:
+            return np.float32(0.75)#np.uint8(200)
+        elif tile == Tile.snake:
+            return np.float32(0.25)#np.uint8(75)
+        else:
+            return np.float32(1.0)#np.uint8(0)

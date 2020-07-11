e0f3fbad7232d15ec60d1e47b6caed13eb325a37
Wyckliffe Aluga <51138208+WyckliffeAluga@users.noreply.github.com>
2020-07-10 21:45:45 -0700

    Create train.py

---
+++ ai-tennis/train.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  6 00:16:36 2020
+
+@author: wyckliffe
+"""
+
+from agent import Agent
+from collections import deque
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import time
+import torch
+from unityagents import UnityEnvironment
